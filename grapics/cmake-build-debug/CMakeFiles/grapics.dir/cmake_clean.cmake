438bb30e7fb54578833725ad3b4401856ffec0dd
coderNamedPaul <76406501+coderNamedPaul@users.noreply.github.com>
2021-01-05 23:46:49 +0300

    Create cmake_clean.cmake

---
+++ grapics/cmake-build-debug/CMakeFiles/grapics.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/grapics.dir/main.cpp.obj"
+  "grapics.exe"
+  "grapics.exe.manifest"
+  "grapics.pdb"
+  "libgrapics.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/grapics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
